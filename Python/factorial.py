62da1e49b5cab7de4b3fcebb145aeac6b528fe3d
Agrima Ojha <agrimaojha@gmail.com>
2025-10-28 00:30:01 +0530

    Factorial code turned into function

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,19 +1,9 @@
-# Python program to find the factorial of a number provided by the user.
-
-# change the value for a different result
-num = 7
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+def factorial(n):#defining a function factorial
+    if n<0:
+        return "Sorry, factorial does not exist for negative numbers."
+    elif n==0 or n==1: #base case 
+        return 1
+    else:
+        return n*factorial(n-1) #recrusive code
+num=int(input())
+print(f"The factorial of {num} is {factorial(num)}")
